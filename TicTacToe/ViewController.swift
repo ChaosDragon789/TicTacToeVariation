094e96b4eea205f1bf0e7c12ad78da24b30c6a20
Ethan Chang <EChang1@ethans-macbook-air-4.local>
2018-10-29 11:15:25 -0500

    Initial Commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Ethan Chang on 10/29/18.
+//  Copyright © 2018 SomeAweApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
